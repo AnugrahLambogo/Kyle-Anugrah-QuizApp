3b7b61aab7444ee0e182aedc53adb25b27d8cf65
Kyle Jensen <kylenjensen19@gmail.com>
2019-10-28 19:26:16 -0700

    finished the javascript part of the quiz app

--- index.js
+++ index.js
@@ -1,137 +1,155 @@
 //start the quiz by clicking the start quiz button
 function startQuiz(){
-    $(("#start").on("click", function(event){
-        loadsQuestion();
-    }))
+  $('.start').on('click', function(event){
+    console.log(`startQuiz runs!!`);
+    loadsQuestions();
+  });
 }
 //function that updates the question number and the score
 function updatesQuestionAndScore(){
-    const qNumberScore = $(`<ul> <li id ="js-qNumber">Question: 
-    ${STORE.currentQuestion + 1}/${STORE.questions.length}</li>
-    <li id ="js-qScore">Score: ${STORE.score}/${STORE.questions.length}</li>
-    </ul>`)
-    $(".q-and-s").html(html);
+  console.log("updatesQuestionAndScore runs!!");
+  const html = $(`<ul>
+  <li id="js-answered">Questions Number: ${STORE.currentQuestion + 1}/${STORE.questions.length}</li>
+      <li id="js-score">Score: ${STORE.score}/${STORE.questions.length}</li>
+    </ul>`);
+  $(".question-and-score").html(html);
 }
 //function that loads in the answer options
 function answerOptions(){
-    let question = STORE.questions
-    [STORE.currentQuestion];
-    for(i = 0; i < questions.options.length; i++){
-        $('.js-options').append(`
-        <input type = "radio" name="options" id="option${i+1}" value= "${question.options[i]}" tabindex ="${i+1}"> 
-        <label for="option${i+1}"> ${question.options[i]}</label> <br/>
-        <span id="js-r${i+1}"></span>
-    `);
-
-        )
-    }
+  console.log("answerOptions runs!!");
+  let question = STORE.questions[STORE.currentQuestion];
+  for(i =0; i < questions.options.length; i++){
+    $('js-options').append(
+      `input type ="radio name" ="options"
+          id = "options${i+1}" value ="${question.options[i]}"
+          tabindex ="${i + 1}">
+          <label for ="option${i+1}"> ${question.options[i]}
+          </label> <br/>
+          <span id ="js-r${i+1}</span>
+          `);
+  }
 }
 //function to load the questions and adds Html
 function loadsQuestions(){
-    let question = STORE.questions
-    [STORE.currentQuestion];
-    updatesQuestionAndScore();
-    const htmlQuestion = $(
-        `<div>
-    <form id="js-questions" class="question-form">
-      
-      <fieldset>
-        <div class="row question">
-          <div class="space">
-            <legend> ${question.question}</legend>
+  console.log("loadsQuestions works!!");
+  let question = STORE.questions[STORE.currentQuestion];
+  updatesQuestionAndScore();
+  const questionHtml = $(
+    `<div>
+        <form id = "teddy-questions" class = "question-form">
+        
+     <fieldset>
+        <div class="line question">
+         <div class ="context">
+           <legend>${question.question}</legend>
+         </div>
+        </div>
+
+        <div class ="line options">
+         <div class ="context">
+          <div class ="js-options"></div>
+         </div>
+        </div>
+
+        <div class ="line">
+          <div class ="context">
+          <button type = "submit" id ="answer"
+          tabindex="4">Submit>></button>
+          <button type ="button" id="next-question"
+          tabindex ="5">Continue>></button>
           </div>
         </div>
+     </fieldset>
+         </form>
+    </div>`);
+  $("main").html(questionHtml);
+  answerOptions();
+  $("#next-question").hide();
 
-        <div class="row options">
-          <div class="space">
-            <div class="js-options"> </div>
-        </div>
-      </div>
-    
 
-      <div class="row">
-        <div class="space">
-          <button type = "submit" id="answer" tabindex="5">Submit</button>
-          <button type = "button" id="next-question" tabindex="6"> Next >></button>
-        </div>
-      </div>
-    </fieldset>
-    </form>
-  </div>`
-    );
-    $("main").html(htmlQuestion);
-    answerOptions();
-    $("#next-question").hide();
 }
 //this function checks if the questions are at the end of the list
 function endList(){
-    $('body').on('click','#next-question', (event) => {
-        STORE.currentQuestion === STORE.questions.length?displayResults() : renderAQuestion();
-      });
-}
-//function that checks if the answer is right or wrong
-function checkAnswer(){
-    $('body').on("submit",'#js-questions', function(event) {
-        event.preventDefault();
-        let currentQues = STORE.questions[STORE.currentQuestion];
-        let selectedOption = $("input[name=options]:checked").val();
-        if (!selectedOption) {
-          alert("Choose an option");
-          return;
-        } 
-        let id_num = currentQues.options.findIndex(i => i === selectedOption);
-        let id = "#js-r" + ++id_num;
-        $('span').removeClass("right-answer wrong-answer");
-        if(selectedOption === currentQues.answer) {
-          STORE.score++; 
-          $(`${id}`).append(`You got it right<br/>`);
-          $(`${id}`).addClass("right-answer");
-        }
-        else {
-          $(`${id}`).append(`You got it wrong <br/> The answer is "${currentQues.answer}"<br/>`);
-          $(`${id}`).addClass("wrong-answer");
-        }
-    
-        STORE.currentQuestion++;
-        $("#js-score").text(`Score: ${STORE.score}/${STORE.questions.length}`);
-        $('#answer').hide();
-        $("input[type=radio]").attr('disabled', true);
-        $('#next-question').show();
-      });
+  console.log("endList works");
+  $('body').on('click','#next-question', (event) => {
+    STORE.currentQuestion === STORE.questions.length?
+      quizResults() : loadsQuestions();
+  });
 }
 //function that shows the result of the quiz
 function quizResults(){
-    let resultHtml = $(
-        `<div class="results">
-          <form id="js-restart-quiz">
-            <fieldset>
-              <div class="row">
-                <div class="col-12">
-                  <legend>Your Score is: ${STORE.score}/${STORE.questions.length}</legend>
-                </div>
-              </div>
-            
-              <div class="row">
-                <div class="col-12">
-                  <button type="button" id="restart"> Restart Quiz </button>
-                </div>
-              </div>
-            </fieldset>
+    console.log("quizResults works!!");
+    let testResult = $(`
+  <div class ="results">
+     <form id ="js-restart-quiz">
+        <fieldset>
+          <div class="line">
+            <div class ="context">
+              <legend>Good Job! This is how well you know Teddy:
+              ${STORE.score}/${STORE.questions.length}</legend>
+             </div>
+          </div>
+
+          <div class ="line">
+           <div class ="context">
+            <button type ="button" id ="restart">
+             Restart the quiz </button>
+           </div>  
+           </div>
+        </fieldset>
         </form>
-        </div>`);
-        STORE.currentQuestion = 0;
-        STORE.score = 0;
-      $("main").html(resultHtml);
-    }
+    </div>`);
+    $("main").html(testResult);
+    answerOptions();
+    $("#next-question").hide();
+      
+  }
+//function that checks if the answer is right or wrong
+function checkAnswer(){
+  console.log("checkAnswers works!!");
+  $('body').on("submit", '#js-questions', function
+  (event) {
+      event.preventDefault();
+      let currentQues = STORE.questions
+      [STORE.currentQuestion];
+      let selectedOption = $("input[name=options]:checked").val();
+      if(!selectedOption){
+          alert("Teddy doesn't take no for an answer!!");
+          return;
+      }
+      let idNum = currentQues.options.findIndex(i => i === selectedOption);
+      let id = "#js-r" + ++idNum;
+      $('span').removeClass("right-answer wrong-answer");
+      if(selectedOption === currentQues.answer){
+          STORE.score++;
+          $(`${id}`).append(`You made Teddy proud!</br>`);
+          $(`${id}`).addClass("right-answer");
+      }
+      else{
+          $(`${id}`).append('You made Ted mad!');
+          $(`${id}`).addClass("wrong-answer");
+      }
+      STORE.currentQuestion++;
+      $("#js-score").text(`Score: ${STORE.score}/
+      ${STORE.questions.length}`);
+      $('#answer').hide();
+      $("input[type=radio]").attr('disabled', true);
+    $('#next-question').show();
+
+  });
+  
 }
-//function that checks if the answer was submitted
-function submitAnswer(){}
-// this function restarts a new quiz
-function restartQuiz(){}
+function restartQuiz(){
+    $('body').on('click','#restart', (event) => {
+        loadsQuestions();
+    });
+}
+
 //this function handles and loads all the functions for the quiz
-function handlesQuiz(){
- startQuiz();
- checkAnswer();
- endList();
- restartQuiz(); 
+function handleQuiz(){
+  startQuiz();
+  endList();
+  checkAnswer();
+  restartQuiz();
 }
+$(handleQuiz);
--- store.js
+++ store.js
@@ -1,71 +1,72 @@
+'use strict'
 const STORE = {
-    questions [//question 1
-        {
-            question:"When is Teddy Roosevelt's Birthday?",
-            options:[
-            "October 25, 1845",
-            "April 20, 1993",
-            "October 27, 1858",
-            "February 19, 1836"
-            ],
-            answer:"October 27, 1858"        
-        },
-        {//question 2
-            question:"How did he become President?",
-            options:[
-                "A rigged election between parties",
-                "He took over after William Mckineley's assassination",
-                "He arm wrestled his candidates and won",
-                "He won with the support of his party."
+  questions: [ //question 1
+    {
+      question:'When is Teddy Roosevelt\'s Birthday?',
+      options:[
+        'October 25, 1845',
+        'April 20, 1993',
+        'October 27, 1858',
+        'February 19, 1836'
+      ],
+      answer:'October 27, 1858'        
+    },
+    {//question 2
+      question:'How did he become President?',
+      options:[
+        'A rigged election between parties',
+        'He took over after William Mckineley\'s assassination',
+        'He arm wrestled his candidates and won',
+        'He won with the support of his party.'
 
-            ],
-            anwser:"He took over after William Mckineley's assassination"      
-        },
-        {//question 3
-            question:"Teddy Roosevelt served in the military during 1896 in what position?",
-            options:[
-                "Assistant Secretary of the Navy",
-                "A General in the Army",
-                "A assistant cook in the Navy",
-                "Served as a Secretary of the Marines"
+      ],
+      anwser:'He took over after William Mckineley\'s assassination'      
+    },
+    {//question 3
+      question:'Teddy Roosevelt served in the military during 1896 in what position?',
+      options:[
+        'Assistant Secretary of the Navy',
+        'A General in the Army',
+        'A assistant cook in the Navy',
+        'Served as a Secretary of the Marines'
 
-            ],  
-            answer:"Assistant Secretary of the Navy"     
-        },
-        {//question 4
-            question:"What product was nicknamed after him?",
-            options:[
-                 "the WiiU",
-                 "toy soldier",
-                 "TED talks",
-                 "teddy bear"
+      ],  
+      answer:'Assistant Secretary of the Navy'     
+    },
+    {//question 4
+      question:'What product was nicknamed after him?',
+      options:[
+        'the WiiU',
+        'toy soldier',
+        'TED talks',
+        'teddy bear'
 
-            ],
-            answer:"teddy bear"       
-        },
-        {//question 5
-            question:"What two acts was congressional acts was Teddy famous for?",
-            options:[
-                "Civil Rights Act and Civil Housing Act",
-                "Prop 8 and Prop 6",
-                "18th Amendent and 19th Amendment",
-                "Pure Food and Drug Act and the Sherman Anti-Trust Act"
+      ],
+      answer:'teddy bear'       
+    },
+    {//question 5
+      question:'What two acts was congressional acts was Teddy famous for?',
+      options:[
+        'Civil Rights Act and Civil Housing Act',
+        'Prop 8 and Prop 6',
+        '18th Amendent and 19th Amendment',
+        'Pure Food and Drug Act and the Sherman Anti-Trust Act'
 
-            ],
-            answer:"Pure Food and Drug Act and the Sherman Anti-Trust Act"  
-        },
-        {//question 6
-            question:"What was the quote Teddy Roosevelt when dealing with foreign politics?",
-            options: [
-                "Beat them with a Big Stick",
-                "Speak softly and carry a Big Stick",
-                "Speak loudly and carry a small stick",
-                "More money, more problems"
+      ],
+      answer:'Pure Food and Drug Act and the Sherman Anti-Trust Act'  
+    },
+    {//question 6
+      question:'What was the quote Teddy Roosevelt when dealing with foreign politics?',
+      options: [
+        'Beat them with a Big Stick',
+        'Speak softly and carry a Big Stick',
+        'Speak loudly and carry a small stick',
+        'More money, more problems'
 
-            ],
-            answer:"Speak softly and carry a Big Stick"      
-        },
-    ],
-    currentQuestion: 0,
-    score:0
-    };
+      ],
+      answer:'Speak softly and carry a Big Stick'      
+    },
+  ],
+  currentQuestion: 0,
+  score:0
+};
